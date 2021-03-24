7b081481db492856d270ec6318ad6b4e597c151b
tom9672 <22634156@student.uwa.edu.au>
2021-03-24 09:18:24 +0800

    import 01-03 workshop code

---
+++ workshop/src/test/java/zlm/workshop-01-code/CalculatorSimpleTest.java
@@ -0,0 +1,64 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorSimpleTest {
+  /**
+   * Sets up the test fixture.
+   *
+   * Called before every test case method.
+   */
+  @BeforeEach
+  public void setUp()
+  {
+  }
+
+  /**
+   * Tears down the test fixture.
+   *
+   * Called after every test case method.
+   */
+  @AfterEach
+  public void tearDown()
+  {
+  }
+
+  @Test
+  public void testAdd() {
+      Calculator c = new Calculator(3, 4);
+      int result = c.add();
+      assertEquals(7, result, "result should be 7");
+  }
+
+  @Test
+  public void testSubtract() {
+      Calculator c = new Calculator(7, 5);
+      int result = c.subtract();
+      assertEquals(2, result, "result should be 2");
+  }
+
+  @Test
+  public void testSubtractThrowsException() {
+      Calculator c = new Calculator(3, 4);
+      Throwable exception = assertThrows(
+          ArithmeticException.class, 
+          () -> c.subtract()
+      );
+  }
+
+  /** Adding zero to a number should always
+    * give us the same number back.
+    */
+  @ParameterizedTest
+  @ValueSource(    ints = { -99, -1, 0, 1, 2, 101, 337  })
+  void addZeroHasNoEffect(int num) {
+      Calculator c = new Calculator(num, 0);
+      int result = c.add();
+      assertEquals(num, result, "result should be same as num");
+  }
+
+}
---
+++ workshop/src/test/java/zlm/workshop-03-code/SalesItemTest.java
@@ -0,0 +1,79 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import java.util.stream.*;
+
+/**
+ * The test class SalesItemTest.
+ *
+ * @author  mik
+ * @version 0.1
+ */
+public class SalesItemTest
+{
+    /**
+     * Default constructor for test class SalesItemTest
+     */
+    public SalesItemTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test that a comment can be added, and that the comment count is correct afterwards.
+     */
+    @Test
+    public void testAddComment()
+    {
+      // TODO
+    }
+
+    /**
+     * Test that a comment using an illegal rating value is rejected.
+     */
+    @Test
+    public void testIllegalRating()
+    {
+        // TODO
+    }
+
+    /**
+     * Test that a sales item is correctly initialised (name and price).
+     */
+    @Test
+    public void testInit()
+    {
+        // TODO
+    }
+
+
+}
+
+
+
